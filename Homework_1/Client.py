bea504668c9cf9ce943c95457388a58cd2544245
Alpkant <kantarcia@itu.edu.tr>
2018-10-25 13:13:39 +0300

    Finalized

--- Homework_1/Client.py
+++ Homework_1/Client.py
@@ -1,61 +1,62 @@
 from socket import *
-import threading
+import threading,os,sys
 
 
 class Client():
 	def receiveMessage(self,socket):
-		while self.is_running and self.is_server_up:
+		while True:
 			try:
 				message = socket.recv(1024).decode()
 				if(message is ""): # If socket closed it will return EOF. Server cannot send EOF via stream
-					self.is_server_up = False
-					self.is_running = False
-					exit(1)
-				print(message)
+					print("Server connection is failed. You will be disconnected...")
+					os._exit(1)
+				print(message) # If it is successful print to the user
 			except:
-				print("Server connection is failed. You will be disconnected.")
-				self.is_running = False
-				self.is_server_up = False
-				exit(0)
+				print("Server connection is failed. You will be disconnected...")
+				socket.close()
+				os._exit(0)
+
 
 
 	def sendMessage(self,socket):
-		while self.is_running and self.is_server_up:
+		while True:
 			message = input()
-			if message is 'exit':
-				self.is_running = False
-				self.is_server_up = False
+			# Remove the spaces
+			message.strip()
+			if message == "exit" :
+				print("Thank you for using chat application. You will be disconnected...")
 				socket.send(message.encode())
 				socket.close()
-				exit(0)
-			
-			socket.send(message.encode())
+				os._exit(0)
+			elif message == "":
+				# If message is empty do not send it because streams do not accept empty strings
+				print("You cannot send empty message.") 
+			else:	
+				socket.send(message.encode())
 
-	def __init__(self,username,server_ip,server_port):
+	def __init__(self,server_ip,server_port):
 		# Set the properties
-		self.is_running = False
-		self.is_server_up = False
-		self.username = username
+		#self.username = username
 		self.server_ip = server_ip
 		self.server_port = server_port
 
 		client_socket = socket(AF_INET,SOCK_STREAM)
-		client_socket.connect((self.server_ip,self.server_port))
-		self.is_server_up = True
-
-		client_socket.send(self.username.encode())
-		self.is_running = True
-		threading.Thread(target = self.receiveMessage, args = (client_socket,)).start()
-		threading.Thread(target = self.sendMessage, args=(client_socket,)).start()
+		try:
+			client_socket.connect((self.server_ip,self.server_port))
+			#client_socket.send(self.username.encode())
+			threading.Thread(target = self.receiveMessage, args = (client_socket,)).start()
+			threading.Thread(target = self.sendMessage, args=(client_socket,)).start()
+		except:
+			print("Connection couldn't be established with the server. Try again later.")
+			os._exit(1)
 
 
 
 
 if __name__ == "__main__":
-	username = input("Enter your username: ")
 	server_ip = "160.75.196.214"
-	if server_ip is '':
+	if server_ip is "":
 		server_ip = input("There is no default server. Enter the server ip that you want to connect: ")
+	#username = input("Enter your username: ")
 	server_port = 12000
-
-	Client(username,server_ip,server_port)
+	Client(server_ip,server_port)
--- Homework_1/Server.py
+++ Homework_1/Server.py
@@ -8,15 +8,30 @@ class Server():
     def listenClient(self,client,addr,client_name):
         while True:
                 message = client.recv(1024).decode()
-                if message=="exit":
+                if message=="exit" : # If user decide to exit
                     print (addr , " is disconnected")
+                    stamp = t.datetime.now().strftime('%H:%M:%S %p') # Get the current time
+                    message = client_name + " is disconnected from the chat " + " @ "   + stamp
+                    # Notify the users that this client is disconnected
+                    for (name,sckt) in self.client_names.items():
+                        if name is not client_name:
+                             sckt.send(message.encode())
+                    # Remove the client from the server user list
                     self.client_names.pop(client_name)
+                    # Close the socket
                     client.close()
                     exit(0)
+                elif message == "" : # If connection closed unexpectedly
+                	print (addr , " is disconnected") 
+                	self.client_names.pop(client_name)
+                	client.close()
+                	exit(1)
                 else:
-                    print (addr , " says: ", message)
+                    print (addr , " says: ", message) # Print to the console 
                     stamp = t.datetime.now().strftime('%H:%M:%S %p')
                     message = client_name + " @ " + stamp + " >> " + message 
+                    # Send message to all clients but not to the sender
+                    # Names are the usernames and sckt are the sockets of the users
                     for (name,sckt) in self.client_names.items():
                         if name is not client_name:
                              sckt.send(message.encode())
@@ -33,11 +48,9 @@ class Server():
             exit(1)
             
         print ("Socket is successfully created.")
-
         try:
             serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         except:
-    
             print ("Socket cannot be used!")
             exit(1)
 
@@ -46,16 +59,13 @@ class Server():
         try:
             serverSocket.bind(('',serverPort))
         except:
-        
             print ("Binding is failed!")
             exit(1)
 
         print ("Binding is done.")
-
         try:
             serverSocket.listen(50)
         except:
-    
             print ("Server is failed to listen!")
             exit(1)
 
@@ -63,26 +73,37 @@ class Server():
 
 
         while True:
-
+        	# Accept the incoming requests
             connectionSocket,addr = serverSocket.accept()
             print('%s is connected to the server' %str(addr))
+            # List all the usernames to the new user
+            connectionSocket.send("Welcome to the server. Here is users in the chat room:\n".encode())
+            clientlist = ""
+            for i in self.client_names.keys():
+            	clientlist += i + "\n"
+            
+            if clientlist is "":
+                clientlist = "*No user found*\n"
+            connectionSocket.send(clientlist.encode())
+            # Notify the user because user should choose username
+            connectionSocket.send("Please choose username:".encode())
             client_name = connectionSocket.recv(1024).decode()
+            client_name = client_name.strip()
             if client_name not in self.client_names:
-                print('%s is claimed %s username' %(str(addr) , str(client_name)))
+            	# If user selected appropriate username then it can start to chat with others
+                print('%s is claimed %s as its username' %(str(addr) , str(client_name)))
+                connectionSocket.send("You have selected your username. You can now chat with the others.".encode())
                 self.client_names[client_name] = connectionSocket
-                connectionSocket.send("Welcome to the server. Here is users in the chat room:\n".encode())
-                clientlist = ""
-                for i in self.client_names.keys():
-                    if(i is not client_name):
-                        clientlist += i + "\n"
-                
-                if clientlist is "":
-                    clientlist = "*No user found*\n"
-                connectionSocket.send(clientlist.encode())
                 threading.Thread(target = self.listenClient,args = (connectionSocket,addr,client_name)).start()
-            else:
+            elif client_name is not "":
+            	# User selected wrong username so 
                 connectionSocket.send("There is already user with that name. You will be disconnected.\n".encode())
                 connectionSocket.close()
+                exit(1)
+            else:
+            	connectionSocket.send("You have selected invalid username. You will be disconnected.\n".encode())
+            	connectionSocket.close()
+            	exit(1)
 
 
 if __name__=="__main__":
